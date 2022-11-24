d3b65d4ae7ea49d1b2048ad525fc17f9eda50a38
swartsm <swartsm#oregonstate.edu>
2022-11-23 20:20:54 -0800

    module not found error

--- photo_capture.test.py
+++ photo_capture.test.py
@@ -1,6 +1,7 @@
 import unittest
 import firebase_admin
 from firebase_admin import firestore
+import photo_capture
 from photo_capture import identify_user
 
 
@@ -9,3 +10,5 @@ class TestCase(unittest.TestCase):
         message = "something"
         self.assertTrue(identify_user('imageFromDB.jpg'))
 
+if __name__ == "__main__":
+    unittest.main(verbosity=2,failfast=True)
