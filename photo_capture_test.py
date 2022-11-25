7a210e89d24f23c9beb5b5ecf53fdf92dc108a64
swartsm <swartsm#oregonstate.edu>
2022-11-24 20:39:19 -0800

    redirect user to login after successful registration

--- photo_capture_test.py
+++ photo_capture_test.py
@@ -1,12 +1,19 @@
 import unittest
 import photo_capture
 from photo_capture import identify_user
+from photo_capture import convert_to_byte_array
+from photo_capture import convert_to_image
 
 
-class TestCase(unittest.TestCase):
+class PhotoTestCase(unittest.TestCase):
     def test1(self):
-        message = "something"
         self.assertTrue(identify_user('imageFromDB.jpg'))
 
+    def test2(self):
+        self.assertIsInstance(convert_to_byte_array('person.jpg'), bytes)
+
+    def test3(self):
+        pass
+
 if __name__ == "__main__":
-    unittest.main(verbosity=2,failfast=True)
+    unittest.main(verbosity=2)
